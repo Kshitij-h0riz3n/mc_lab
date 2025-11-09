fb18cc77e2da2cea449676e84b7d44944bbb8270
Kshitij Shrawade <kshitijshrawade@gmail.com>
2025-11-10 00:41:56 +0530

    Refactor delay function and update main loop
    Refactored delay function to delay_ms for millisecond precision and updated main loop to use it.

--- 8051/7seg_8051.c
+++ 8051/7seg_8051.c
@@ -1,26 +1,29 @@
 #include <reg51.h>
 
-void delay(){
-    int i;
-    for(i=0; i<=12; i++){
-        TMOD = 0x01;
-        TL0 = 0x00;
-        TH0 = 0x00;
+void delay_ms(unsigned int ms) {
+    unsigned int i;
+    for (i = 0; i < ms; i++) {
+        TH0 = 0xFC;
+        TL0 = 0x66;
         TR0 = 1;
-        while(TF0 == 0);
+        while (TF0 == 1);
+        while (TF0 == 0);
         TR0 = 0;
         TF0 = 0;
-    }    
+    }
 }
 
-unsigned char codes[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f}
+unsigned char codes[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f};
 
-void main(){
+void main(void) {
     int k;
-    while(1){
-        for(k=0; k<11; k++){
+    TMOD = (TMOD & 0xF0) | 0x01;
+    TF0 = 0;
+    TR0 = 0;
+    while (1) {
+        for (k = 0; k < 10; k++) {
             P2 = codes[k];
-            delay();
+            delay_ms(500);
         }
     }
 }
